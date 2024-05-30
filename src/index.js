70a3aad76532e6d4f8df5efad8bd049ccdd79ccf
eod940 <eod940@gmail.com>
2024-05-30 22:29:28 +0900

    feat: 시계 만들기 1 - ReactDOM.render() 함수 deprecated 이슈

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from "./chapter03/Library";
+import Clock from "./chapter04/Clock";
+import {createRoot} from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
+setInterval(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Clock/>
+    </React.StrictMode>,
     document.getElementById('root')
-);
+  );
+}, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
